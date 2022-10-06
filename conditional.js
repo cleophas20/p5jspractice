3fa2b9fef39957fb0133b0e15934d977a0e46240
Cleophas <cleophasfoy@gmail.com>
2022-10-05 22:17:57 -0500

    added logic for ball bouncing on left and right of canvas

--- conditional.js
+++ conditional.js
@@ -1,16 +1,22 @@
+var x = 0;
+var speed = 3;
+
 function setup() {
   createCanvas(600, 400);
 }
 
 function draw() {
   background(0);
-
   stroke(255);
-  strokeweight(4);
-  noFill():
-
-  if (mouseX > 300) {
-    fill(255, 0, 200);
+  strokeWeight(4);
+  noFill();
+  ellipse(x, 200, 100, 100);
+  if (x > width - 50) {
+    speed =  -3;
   }
-  ellipse(300, 200, 100, 100);
+  if (x < 50) {
+    speed = 3;
+  }
+  x = x + speed;
+
 }
