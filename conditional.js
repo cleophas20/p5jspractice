cf8d1a81ca299d16cf61587af2bc1d6a8b4bc05c
Cleophas <cleophasfoy@gmail.com>
2022-10-07 12:47:50 -0500

    more experiments with conditionals

--- conditional.js
+++ conditional.js
@@ -1,4 +1,4 @@
-var x = 0;
+var x = 51;
 var speed = 3;
 
 function setup() {
@@ -11,12 +11,8 @@ function draw() {
   strokeWeight(4);
   noFill();
   ellipse(x, 200, 100, 100);
-  if (x > width - 50) {
-    speed =  -3;
-  }
-  if (x < 50) {
-    speed = 3;
+  if (x > width - 50 || x < 49) {
+    speed =  speed * -1;
   }
   x = x + speed;
-
 }
