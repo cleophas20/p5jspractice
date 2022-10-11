2389ab38e7e0332ef74a85be9c481d6bae4cebe4
Cleophas <cleophasfoy@gmail.com>
2022-10-11 11:47:16 -0500

    experimented with mouse over functions in classes

--- classes.js
+++ classes.js
@@ -1,38 +1,42 @@
-let bubble;
+let pop;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  bubble = new Bubble();
+  pop1 = new Pop(100, 100, 100);
+  pop2 = new Pop(500, 500, 200);
+  pop3 = new Pop(700, 150, 400);
 }
 
 function draw() {
   background(0);
-  bubble.move();
-  bubble.show();
+//  pop.move();
+  pop1.show();
+  pop2.show();
+  pop3.show();
 }
 
-class Bubble {
-  constructor() {
-    this.x = this.x + random(-5, 5);
-    this.y = this.y + random(-5, 5);
+class Pop {
+  constructor(startX, startY, size) {
+    this.x = startX;
+    this.y = startY;
+    this.z = size
+
   }
-  move() {
-    this.x
-  }
+  // move() {
+  //   this.x = this.x + random(-5, 5);
+  //   this.y = this.y + random(-5, 5);
+//  }
   show() {
     stroke(255);
     strokeWeight(4);
-    noFill();
-    ellipse(this.x, this.y, 24, 24);
+    if (mouseX > this.x - (this.z/2) &&
+    mouseX < this.x + (this.z/2) &&
+    mouseY > this.y - (this.z/2) &&
+    mouseY < this.y + (this.z/2)) {
+      fill(255)
+    } else noFill();
+    ellipse(this.x, this.y, this.z, this.z);
   }
-}
 
-// function show() {
-//   stroke(255);
-//   strokWeight(4);
-//
-// }
-//
-// function move() {
-//
-// }
+
+}
