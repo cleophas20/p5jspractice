147865aad2ad0317f730e1286035a62cc53353ac
Cleophas <cleophasfoy@gmail.com>
2022-10-11 11:16:14 -0500

    experimenting with classes

--- classes.js
+++ classes.js
@@ -1,7 +1,38 @@
+let bubble;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  bubble = new Bubble();
 }
 
 function draw() {
-  
+  background(0);
+  bubble.move();
+  bubble.show();
 }
+
+class Bubble {
+  constructor() {
+    this.x = this.x + random(-5, 5);
+    this.y = this.y + random(-5, 5);
+  }
+  move() {
+    this.x
+  }
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, 24, 24);
+  }
+}
+
+// function show() {
+//   stroke(255);
+//   strokWeight(4);
+//
+// }
+//
+// function move() {
+//
+// }
